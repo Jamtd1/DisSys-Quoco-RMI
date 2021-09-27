91d71725fe4d00a28b4500d276b6add567454df8
Jennifer Daly <jenniferdaly94@gmail.com>
2021-09-27 17:33:20 +0100

    First commit of practical 1 for distributed systems.

---
+++ quoco-rmi/auldfellas/src/test/java/AuldfellasUnitTest.java
@@ -0,0 +1,49 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+// import service.core.ClientInfo;
+// import service.core.Constants;
+// import service.core.Quotation;
+// import service.core.QuotationService;
+// import service.auldfellas.AFQService;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AuldfellasUnitTest {
+    private static Registry registry;
+
+    @BeforeClass
+    public static void setup() {
+        QuotationService afqService = new AFQService();
+        try {
+            registry = LocateRegistry.createRegistry(1099);
+
+            QuotationService quotationService = (QuotationService)
+                UnicastRemoteObject.exportObject(afqService,0);
+
+            registry.bind(Constants.AULD_FELLAS_SERVICE, quotationService);
+        } catch (Exception e) {
+            System.out.println("Trouble: " + e);
+        }
+    }
+    @Test
+    public void connectionTest() throws Exception {
+        QuotationService service = (QuotationService)
+            registry.lookup(Constants.AULD_FELLAS_SERVICE);
+        assertNotNull(service);
+    }
+
+    @Test
+    public void generationTest() throws Exception {
+        QuotationService service = (QuotationService)
+            registry.lookup(Constants.AULD_FELLAS_SERVICE);
+
+        ClientInfo info = new ClientInfo("Test", 'F', 61, 3, 0, "122-d-7890");
+        
+        Quotation quotation = service.generateQuotation(info);
+        assertEquals("The price is correct", 640.92, quotation.price, 0);
+    }
+}
